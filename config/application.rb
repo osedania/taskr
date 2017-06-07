0074a81543e9c5f8237e185a714ccbf6d2482dd1
junydania <o.dania@aol.com>
2017-06-07 17:26:49 +0200

    setup cucumber and rspec testing frameworks

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,15 @@ module Taskr
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |generate|
+    generate.helper false
+    generate.assets false
+    generate.view_specs false
+    generate.helper_specs false
+    generate.routing_specs false
+    generate.controller_specs false
+  end
+
   end
 end
