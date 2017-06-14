af506d5b632ea4875b1e143cf89e72c7042396a2
junydania <o.dania@aol.com>
2017-06-14 12:31:43 +0200

    Devise user model generated and rspec pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613085539) do
+ActiveRecord::Schema.define(version: 20170614100331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,38 @@ ActiveRecord::Schema.define(version: 20170613085539) do
     t.string "status"
     t.index ["requester_id"], name: "index_tasks_on_requester_id"
     t.index ["task_category_id"], name: "index_tasks_on_task_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company_name"
+    t.string "insurance"
+    t.string "contractor_description"
+    t.string "user_name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "country"
+    t.string "contractor_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "requester"
+    t.boolean "contractor"
+    t.boolean "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bids", "contractors"
